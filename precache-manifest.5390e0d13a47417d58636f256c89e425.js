d1cf757ffd0074bff74ed36a4912ff5773562123
unknown <irankundapatrick01@gmail.com>
2020-09-11 12:36:11 +0200

    Updates

---
+++ precache-manifest.5390e0d13a47417d58636f256c89e425.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d69feeca59015f424a5f36bdd029469",
+    "url": "/React-To-Do-App/index.html"
+  },
+  {
+    "revision": "d60c02c11b8bb6c4301a",
+    "url": "/React-To-Do-App/static/css/main.6fe0fcc3.chunk.css"
+  },
+  {
+    "revision": "5db7f2d4b03ed35435c7",
+    "url": "/React-To-Do-App/static/js/2.03b3a3f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-To-Do-App/static/js/2.03b3a3f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d60c02c11b8bb6c4301a",
+    "url": "/React-To-Do-App/static/js/main.bfaefd68.chunk.js"
+  },
+  {
+    "revision": "5364f329f49e991249ab",
+    "url": "/React-To-Do-App/static/js/runtime-main.0569825c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-To-Do-App/static/media/logo.5d5d9eef.svg"
+  }
+]);
